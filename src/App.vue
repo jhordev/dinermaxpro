67f2210a728283d0970f7056f18316bcd1d6c785
Jhordy Mondragon Saucedo <jhordymondragon4@gmail.com>
2024-12-09 11:03:28 -0500

    Estructura inicial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/img/isotipo.png" width="150" height="150" />
+
+    <div class="wrapper">
+      <HelloWorld msg="DINERMAX" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+
+</template>
+
+<style scoped>
+
+</style>
